166f43c807498a31f3d585db00a8ac8c585e6fd7
NikhilN03 <nikhil.c.netto@gmail.com>
2025-09-01 00:38:42 -0400

    initial springboot skeleton

---
+++ rivals-api/src/test/java/com/rivals/rivals_api/RivalsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rivals.rivals_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RivalsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
